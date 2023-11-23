e965020d56e3b3795a5d954b3e7c08e995575b4d
Callum Newsome <39914113+callumndev@users.noreply.github.com>
2023-11-23 17:14:35 +0000

    Add new home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import Head from "next/head";
-import Link from "next/link";
+import Image from "next/image";
 import type {
     GetServerSidePropsContext,
     GetServerSidePropsResult,
     InferGetServerSidePropsType
 } from "next";
+
+import clsx from 'clsx';
+
+import {
+    ArrowDownTrayIcon,
+    Cog6ToothIcon,
+    FolderOpenIcon,
+    UserPlusIcon
+} from '@heroicons/react/24/outline';
 
 import type { User } from "lucia";
 
@@ -51,7 +60,73 @@ export const getServerSideProps = async (
             user: session.user
         }
     }
-};
+}
+
+const stats = [
+    { label: 'Uploads', value: 416 },
+    { label: 'Last Uploaded', value: '41 minutes ago' },
+    { label: 'Joined', value: '2 years ago' },
+]
+const actions = [
+    {
+        icon: FolderOpenIcon,
+        name: 'My Uploads',
+        text: 'View all the uploads you have submitted.',
+        href: '#',
+        iconForeground: 'text-purple-500',
+        iconBackground: 'bg-purple-50',
+    },
+    {
+        icon: ArrowDownTrayIcon,
+        name: 'Download',
+        text: 'Download the ShareX configuration file (.sxcu) for this site and account.',
+        href: '#',
+        iconForeground: 'text-purple-500',
+        iconBackground: 'bg-purple-50',
+    },
+    {
+        icon: UserPlusIcon,
+        name: 'Registration Requests',
+        text: 'Manage pending registration requests from users. (Admin only)',
+        href: '#',
+        iconForeground: 'text-purple-500',
+        iconBackground: 'bg-purple-50',
+    },
+    {
+        icon: Cog6ToothIcon,
+        name: 'Settings',
+        text: 'Manage the global settings for the site. (Admin only)',
+        href: '#',
+        iconForeground: 'text-purple-500',
+        iconBackground: 'bg-purple-50',
+    },
+]
+const recentUploads = [
+    {
+        name: '999_ash_999',
+        handle: '41 minutes ago',
+        imageUrl: 'https://cdn.discordapp.com/embed/avatars/3.png',
+        href: '#',
+    },
+    {
+        name: 'lzz',
+        handle: '12 hours ago',
+        imageUrl: 'https://cdn.discordapp.com/embed/avatars/1.png',
+        href: '#',
+    },
+    {
+        name: '999_ash_999',
+        handle: '2 days ago',
+        imageUrl: 'https://cdn.discordapp.com/embed/avatars/3.png',
+        href: '#',
+    },
+    {
+        name: 'sam00001',
+        handle: '5 days ago',
+        imageUrl: 'https://cdn.discordapp.com/embed/avatars/2.png',
+        href: '#',
+    },
+]
 
 export default function Home(
     { user }:
@@ -66,37 +141,169 @@ export default function Home(
                     <link rel="icon" href="/favicon.ico" />
                 </Head>
 
-                <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-                    <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-                        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-                            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-                        </h1>
-                        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-                            <Link
-                                className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-                                href="https://create.t3.gg/en/usage/first-steps"
-                                target="_blank"
-                            >
-                                <h3 className="text-2xl font-bold">First Steps →</h3>
-                                <div className="text-lg">
-                                    Just the basics - Everything you need to know to set up your
-                                    database and authentication.
+                <div className="min-h-full">
+                    <main className="mt-10 pb-8">
+                        <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+                            <h1 className="sr-only">Profile</h1>
+                            {/* Main 3 column grid */}
+                            <div className="grid grid-cols-1 items-start gap-4 lg:grid-cols-3 lg:gap-8">
+                                {/* Left column */}
+                                <div className="grid grid-cols-1 gap-4 lg:col-span-2">
+                                    {/* Welcome panel */}
+                                    <section aria-labelledby="profile-overview-title">
+                                        <div className="overflow-hidden rounded-lg bg-white shadow">
+                                            <h2 className="sr-only" id="profile-overview-title">
+                                                Profile Overview
+                                            </h2>
+                                            <div className="bg-white p-6">
+                                                <div className="sm:flex sm:items-center sm:justify-between">
+                                                    <div className="sm:flex sm:space-x-5">
+                                                        <div className="flex-shrink-0">
+                                                            <Image
+                                                                src={user.avatar}
+                                                                alt="user avatar"
+                                                                className="mx-auto h-20 w-20 rounded-full"
+                                                                width={80}
+                                                                height={80}
+                                                            />
+                                                        </div>
+                                                        <div className="mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left">
+                                                            <p className="text-sm font-medium text-gray-600">Welcome back,</p>
+                                                            <p className="text-xl font-bold text-gray-900 sm:text-2xl">{user.discordUsername}</p>
+                                                            <p className="text-sm font-medium text-gray-600">{"Super Admin"}</p>
+                                                        </div>
+                                                    </div>
+                                                    <div className="mt-5 flex justify-center sm:mt-0">
+                                                        <a
+                                                            href="/me/profile"
+                                                            className="flex items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                                                        >
+                                                            View profile
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="grid grid-cols-1 divide-y divide-gray-200 border-t border-gray-200 bg-gray-50 sm:grid-cols-3 sm:divide-x sm:divide-y-0">
+                                                {stats.map((stat) => (
+                                                    <div key={stat.label} className="px-6 py-5 text-center text-sm font-medium">
+                                                        <span className="text-gray-900">{stat.value}</span>{' '}
+                                                        <span className="text-gray-600">{stat.label}</span>
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        </div>
+                                    </section>
+
+                                    {/* Actions panel */}
+                                    <section aria-labelledby="quick-links-title">
+                                        <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-gray-200 shadow sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0">
+                                            <h2 className="sr-only" id="quick-links-title">
+                                                Quick links
+                                            </h2>
+                                            {actions.map((action, actionIdx) => (
+                                                <div
+                                                    key={action.name}
+                                                    className={clsx(
+                                                        actionIdx === 0 ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none' : '',
+                                                        actionIdx === 1 ? 'sm:rounded-tr-lg' : '',
+                                                        actionIdx === actions.length - 2 ? 'sm:rounded-bl-lg' : '',
+                                                        actionIdx === actions.length - 1 ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none' : '',
+                                                        'group relative bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-cyan-500'
+                                                    )}
+                                                >
+                                                    <div>
+                                                        <span
+                                                            className={clsx(
+                                                                action.iconBackground,
+                                                                action.iconForeground,
+                                                                'inline-flex rounded-lg p-3 ring-4 ring-white'
+                                                            )}
+                                                        >
+                                                            <action.icon className="h-6 w-6" aria-hidden="true" />
+                                                        </span>
+                                                    </div>
+                                                    <div className="mt-8">
+                                                        <h3 className="text-lg font-medium">
+                                                            <a href={action.href} className="focus:outline-none">
+                                                                {/* Extend touch target to entire panel */}
+                                                                <span className="absolute inset-0" aria-hidden="true" />
+                                                                {action.name}
+                                                            </a>
+                                                        </h3>
+                                                        <p className="mt-2 text-sm text-gray-500">
+                                                            {action.text}
+                                                        </p>
+                                                    </div>
+                                                    <span
+                                                        className="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400"
+                                                        aria-hidden="true"
+                                                    >
+                                                        <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                                                            <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
+                                                        </svg>
+                                                    </span>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </section>
                                 </div>
-                            </Link>
-                            <Link
-                                className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-                                href="https://create.t3.gg/en/introduction"
-                                target="_blank"
-                            >
-                                <h3 className="text-2xl font-bold">Documentation →</h3>
-                                <div className="text-lg">
-                                    Learn more about Create T3 App, the libraries it uses, and how
-                                    to deploy it.
+
+                                {/* Right column */}
+                                <div className="grid grid-cols-1 gap-4">
+                                    {/* Recent uploads */}
+                                    <section aria-labelledby="recent-uploads-title">
+                                        <div className="overflow-hidden rounded-lg bg-white shadow">
+                                            <div className="p-6">
+                                                <h2 className="text-base font-medium text-gray-900" id="recent-uploads-title">
+                                                    Recent Uploads
+                                                </h2>
+                                                <div className="mt-6 flow-root">
+                                                    <ul role="list" className="-my-5 divide-y divide-gray-200">
+                                                        {recentUploads.map((person) => (
+                                                            <li key={person.handle} className="py-4">
+                                                                <div className="flex items-center space-x-4">
+                                                                    <div className="flex-shrink-0">
+                                                                        <Image
+                                                                            src={person.imageUrl}
+                                                                            alt="person avatar"
+                                                                            className="h-8 w-8 rounded-full"
+                                                                            width={32}
+                                                                            height={32}
+                                                                        />
+                                                                    </div>
+                                                                    <div className="min-w-0 flex-1">
+                                                                        <p className="truncate text-sm font-medium text-gray-900">{person.name}</p>
+                                                                        <p className="truncate text-sm text-gray-500">{person.handle}</p>
+                                                                    </div>
+                                                                    <div>
+                                                                        <a
+                                                                            href={person.href}
+                                                                            className="inline-flex items-center rounded-full bg-white px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                                                                        >
+                                                                            View
+                                                                        </a>
+                                                                    </div>
+                                                                </div>
+                                                            </li>
+                                                        ))}
+                                                    </ul>
+                                                </div>
+                                                <div className="mt-6">
+                                                    <a
+                                                        href="/uploads"
+                                                        className="flex w-full items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                                                    >
+                                                        View all
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </section>
                                 </div>
-                            </Link>
+                            </div>
                         </div>
-                    </div>
-                </main>
+                    </main>
+                </div>
             </Layout>
         </>
     );
